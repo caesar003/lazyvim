837e4cc0f860f2a71b902b84340b76dd1441de21
caesar003 <caesarmuksid@gmail.com>
2023-10-26 16:52:09 +0700

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+
+  -- { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- { "bluz71/vim-nightfly-colors", name = "nightfly" },
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   opts = {
+  --     transparent_mode = true,
+  --   },
+  -- },
+
+  -- {
+  --   "joshdick/onedark.vim",
+  -- },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+}
